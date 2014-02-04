0646118719645fd6afc8d6773521fe7a99471b00
Erin Hamilton <erin@erinhamilton.me>
2014-02-03 21:10:36 -0600

    Moon header with atmosphere

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+/*
+** Switch the active class on the navbar
+*/
+
+$('.navbar li').click(function(e) {
+    $('.navbar li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+    e.preventDefault();
+});
+
+/*
+** Initiate skrollr.js
+*/
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery )
